c857b8afd88b1c8e351874e764d4d3bc21d0d04b
Hackerpilot <brian.schott@economicmodeling.com>
2014-04-09 16:43:44 -0700

    Update headers

--- src/memory/appender.d
+++ src/memory/appender.d
@@ -1,5 +1,8 @@
 /**
  * Tools for building up arrays without using the GC.
+ * Copyright: © 2014 Economic Modeling Specialists, Intl.
+ * Authors: Brian Schott
+ * License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt Boost License 1.0)
  */
 
 module memory.appender;
