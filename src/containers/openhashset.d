c8bc013345378fcf74981cf6b4e45073140e7496
Hackerpilot <briancschott@gmail.com>
2015-08-03 16:42:34 -0700

    Add module declaration

--- src/containers/openhashset.d
+++ src/containers/openhashset.d
@@ -4,6 +4,7 @@
  * Authors: Brian Schott
  * License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
  */
+module containers.openhashset;
 
 import containers.internal.hash : generateHash;
 import containers.internal.node : shouldAddGCRange;
