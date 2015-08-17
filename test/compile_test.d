fb27a583daa680ad8c3b5f7f8bbfc49a12168518
Hackerpilot <briancschott@gmail.com>
2015-08-17 02:03:55 -0700

    More checks

--- test/compile_test.d
+++ test/compile_test.d
@@ -9,68 +9,249 @@ import containers.ttree;
 
 private void testContainerSingle(alias Container)()
 {
-	testContainerSingle2!(Container)();
-	testContainerSingle2!(const(Container))();
-	testContainerSingle2!(immutable(Container))();
+	testContainerSingleVal!(Container)();
+	testContainerSingleRef!(Container)();
 }
 
-private  void testContainerDouble(alias Container)()
+private void testContainerSingleVal(alias Container)()
 {
-	testContainerDouble2!(Container)();
-	testContainerDouble2!(const(Container))();
-	testContainerDouble2!(immutable(Container))();
-}
+	Container!(int) mm;
+	Container!(const int) mc;
+	Container!(immutable int) mi;
 
-private void testContainerSingle2(alias Container)()
-{
-	Container!(int) m;
-	Container!(const int) c;
-	Container!(immutable int) i;
+	const Container!(int) cm;
+	const Container!(const int) cc;
+	const Container!(immutable int) ci;
 
-	checkFunctionality!(int)(m);
-	checkFunctionality!(const int)(c);
-	checkFunctionality!(immutable int)(i);
-}
-
-private void testContainerDouble2(alias Container)()
-{
-	Container!(int, int) mm;
-	Container!(int, const int) mc;
-	Container!(int, immutable int) mi;
-	Container!(const int, int) cm;
-	Container!(const int, const int) cc;
-	Container!(const int, immutable int) ci;
-	Container!(immutable int, int) im;
-	Container!(immutable int, const int) ic;
-	Container!(immutable int, immutable int) ii;
+	immutable Container!(int) im;
+	immutable Container!(const int) ic;
+	immutable Container!(immutable int) ii;
 
 	checkFunctionality!(int)(mm);
-	checkFunctionality!(constint)(mc);
+	checkFunctionality!(const int)(mc);
 	checkFunctionality!(immutable int)(mi);
+
 	checkFunctionality!(int)(cm);
-	checkFunctionality!(constint)(cc);
+	checkFunctionality!(const int)(cc);
 	checkFunctionality!(immutable int)(ci);
+
 	checkFunctionality!(int)(im);
-	checkFunctionality!(constint)(ic);
+	checkFunctionality!(const int)(ic);
 	checkFunctionality!(immutable int)(ii);
+}
+
+private void testContainerSingleRef(alias Container)()
+{
+	Container!(int*) mm;
+	Container!(const int*) mc;
+	Container!(immutable int*) mi;
+
+	const Container!(int*) cm;
+	const Container!(const int*) cc;
+	const Container!(immutable int*) ci;
+
+	immutable Container!(int*) im;
+	immutable Container!(const int*) ic;
+	immutable Container!(immutable int*) ii;
+
+	checkFunctionality!(int*)(mm);
+	checkFunctionality!(const int*)(mc);
+	checkFunctionality!(immutable int*)(mi);
+
+	checkFunctionality!(const int*)(cm);
+	checkFunctionality!(const int*)(cc);
+	checkFunctionality!(immutable int*)(ci);
+
+	checkFunctionality!(immutable int*)(im);
+	checkFunctionality!(immutable int*)(ic);
+	checkFunctionality!(immutable int*)(ii);
+}
+
+private void testContainerDouble(alias Container)()
+{
+	testContainerDoubleVal!(Container)();
+	testContainerDoubleRef!(Container)();
+}
+
+private void testContainerDoubleVal(alias Container)()
+{
+	{
+		Container!(int, int) mmm;
+		Container!(int, const int) mmc;
+		Container!(int, immutable int) mmi;
+
+		Container!(const int, int) mcm;
+		Container!(const int, const int) mcc;
+		Container!(const int, immutable int) mci;
+
+		Container!(immutable int, int) mim;
+		Container!(immutable int, const int) mic;
+		Container!(immutable int, immutable int) mii;
+
+		checkFunctionality!(int)(mmm);
+		checkFunctionality!(const int)(mmc);
+		checkFunctionality!(immutable int)(mmi);
+
+		checkFunctionality!(int)(mcm);
+		checkFunctionality!(const int)(mcc);
+		checkFunctionality!(immutable int)(mci);
+
+		checkFunctionality!(int)(mim);
+		checkFunctionality!(const int)(mic);
+		checkFunctionality!(immutable int)(mii);
+	}
+
+	{
+		const Container!(int, int) cmm;
+		const Container!(int, const int) cmc;
+		const Container!(int, immutable int) cmi;
+
+		const Container!(const int, int) ccm;
+		const Container!(const int, const int) ccc;
+		const Container!(const int, immutable int) cci;
+
+		const Container!(immutable int, int) cim;
+		const Container!(immutable int, const int) cic;
+		const Container!(immutable int, immutable int) cii;
+
+		checkFunctionality!(int)(cmm);
+		checkFunctionality!(const int)(cmc);
+		checkFunctionality!(immutable int)(cmi);
+
+		checkFunctionality!(int)(ccm);
+		checkFunctionality!(const int)(ccc);
+		checkFunctionality!(immutable int)(cci);
+
+		checkFunctionality!(int)(cim);
+		checkFunctionality!(const int)(cic);
+		checkFunctionality!(immutable int)(cii);
+	}
+
+	{
+		immutable Container!(int, int) imm;
+		immutable Container!(int, const int) imc;
+		immutable Container!(int, immutable int) imi;
+
+		immutable Container!(const int, int) icm;
+		immutable Container!(const int, const int) icc;
+		immutable Container!(const int, immutable int) ici;
+
+		immutable Container!(immutable int, int) iim;
+		immutable Container!(immutable int, const int) iic;
+		immutable Container!(immutable int, immutable int) iii;
+
+		checkFunctionality!(int)(imm);
+		checkFunctionality!(const int)(imc);
+		checkFunctionality!(immutable int)(imi);
+
+		checkFunctionality!(int)(icm);
+		checkFunctionality!(const int)(icc);
+		checkFunctionality!(immutable int)(ici);
+
+		checkFunctionality!(int)(iim);
+		checkFunctionality!(const int)(iic);
+		checkFunctionality!(immutable int)(iii);
+	}
+}
+
+private void testContainerDoubleRef(alias Container)()
+{
+	{
+		Container!(int, int*) mmm;
+		Container!(int, const int*) mmc;
+		Container!(int, immutable int*) mmi;
+
+		Container!(const int, int*) mcm;
+		Container!(const int, const int*) mcc;
+		Container!(const int, immutable int*) mci;
+
+		Container!(immutable int, int*) mim;
+		Container!(immutable int, const int*) mic;
+		Container!(immutable int, immutable int*) mii;
+
+		checkFunctionality!(int*)(mmm);
+		checkFunctionality!(const int*)(mmc);
+		checkFunctionality!(immutable int*)(mmi);
+
+		checkFunctionality!(int*)(mcm);
+		checkFunctionality!(const int*)(mcc);
+		checkFunctionality!(immutable int*)(mci);
+
+		checkFunctionality!(int*)(mim);
+		checkFunctionality!(const int*)(mic);
+		checkFunctionality!(immutable int*)(mii);
+	}
+
+	{
+		const Container!(int, int*) cmm;
+		const Container!(int, const int*) cmc;
+		const Container!(int, immutable int*) cmi;
+
+		const Container!(const int, int*) ccm;
+		const Container!(const int, const int*) ccc;
+		const Container!(const int, immutable int*) cci;
+
+		const Container!(immutable int, int*) cim;
+		const Container!(immutable int, const int*) cic;
+		const Container!(immutable int, immutable int*) cii;
+
+		checkFunctionality!(const int*)(cmm);
+		checkFunctionality!(const int*)(cmc);
+		checkFunctionality!(immutable int*)(cmi);
+
+		checkFunctionality!(const int*)(ccm);
+		checkFunctionality!(const int*)(ccc);
+		checkFunctionality!(immutable int*)(cci);
+
+		checkFunctionality!(const int*)(cim);
+		checkFunctionality!(const int*)(cic);
+		checkFunctionality!(immutable int*)(cii);
+	}
+
+	{
+		immutable Container!(int, int*) imm;
+		immutable Container!(int, const int*) imc;
+		immutable Container!(int, immutable int*) imi;
+
+		immutable Container!(const int, int*) icm;
+		immutable Container!(const int, const int*) icc;
+		immutable Container!(const int, immutable int*) ici;
+
+		immutable Container!(immutable int, int*) iim;
+		immutable Container!(immutable int, const int*) iic;
+		immutable Container!(immutable int, immutable int*) iii;
+
+		checkFunctionality!(immutable int*)(imm);
+		checkFunctionality!(immutable int*)(imc);
+		checkFunctionality!(immutable int*)(imi);
+
+		checkFunctionality!(immutable int*)(icm);
+		checkFunctionality!(immutable int*)(icc);
+		checkFunctionality!(immutable int*)(ici);
+
+		checkFunctionality!(immutable int*)(iim);
+		checkFunctionality!(immutable int*)(iic);
+		checkFunctionality!(immutable int*)(iii);
+	}
 }
 
 private void checkFunctionality(Type, Container)(ref Container container)
 {
 	auto r = container[];
+//	pragma(msg, "type of " ~ Container.stringof ~ ".front is " ~ typeof(r.front).stringof);
 	static assert(is(typeof(r.front()) == Type));
 	static assert(is(typeof(container.length) == size_t));
-	assert (container.length == 0);
+	assert(container.length == 0);
 }
 
 unittest
 {
-//	testContainerDouble!(HashMap)();
-//	testContainerDouble!(TreeMap)();
+	//	testContainerDouble!(HashMap)();
+	//	testContainerDouble!(TreeMap)();
 	testContainerSingle!(HashSet)();
 	testContainerSingle!(UnrolledList)();
 	testContainerSingle!(OpenHashSet)();
-//	testContainerSingle!(SimdSet)();
+	//	testContainerSingle!(SimdSet)();
 	testContainerSingle!(SList)();
 	testContainerSingle!(TTree)();
 }
