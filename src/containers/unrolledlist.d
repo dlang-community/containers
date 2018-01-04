0ed30c2626270a18b420bc04143b7a9c2eea0f42
Hackerpilot <briancschott@gmail.com>
2018-01-03 16:50:20 -0800

    Some changes for 2.078.0

--- src/containers/unrolledlist.d
+++ src/containers/unrolledlist.d
@@ -508,7 +508,10 @@ private:
 		size_t nextAvailableIndex() const nothrow pure @safe @nogc
 		{
 			import containers.internal.backwards : bsf;
-			return bsf(~registry);
+			static if (BookkeepingType.sizeof < uint.sizeof)
+				return bsf(~(cast(uint) registry));
+			else
+				return bsf(~registry);
 		}
 
 		void markUsed(size_t index) nothrow pure @safe @nogc
