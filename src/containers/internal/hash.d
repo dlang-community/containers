743255382877cc6cf16ee06f58ca13b13d01bb0a
Hackerpilot <briancschott@gmail.com>
2018-11-08 01:44:25 -0800

    Rework some of the hashing algorithms

--- src/containers/internal/hash.d
+++ src/containers/internal/hash.d
@@ -6,6 +6,8 @@
  */
 module containers.internal.hash;
 
+import std.conv : to;
+
 static if (hash_t.sizeof == 4)
 {
 	hash_t generateHash(T)(const T value)
@@ -34,3 +36,27 @@ else
 		return h;
 	}
 }
+
+/**
+ * Convert a hash code into a valid array index.
+ *
+ * Prams:
+ *     hash = the hash code to be mapped
+ *     len = the length of the array that backs the hash container.
+ */
+size_t hashToIndex(const size_t hash, const size_t len) pure nothrow @nogc @safe
+{
+	import core.bitop : bsr;
+
+	// This magic number taken from
+	// https://probablydance.com/2018/06/16/fibonacci-hashing-the-optimization-that-the-world-forgot-or-a-better-alternative-to-integer-modulo/
+	//
+	// It's amazing how much faster this makes the hash data structures
+	// when faced with low quality hash functions.
+	static if (size_t.sizeof == 8)
+		return (hash * 11_400_714_819_323_198_485UL) >>> (64 - bsr(len));
+	else
+		return (hash * 2_654_435_769U) >>> (32 - bsr(len));
+}
+
+enum size_t DEFAULT_BUCKET_COUNT = 8;
--- test/hashmap_gc_test.d
+++ test/hashmap_gc_test.d
@@ -1,48 +1,46 @@
-
 import containers : HashMap;
 import std.stdio : writefln;
 import core.memory : GC;
 
-
 /**
  * Generate a random alphanumeric string.
  */
-@trusted
-string randomString (uint len)
+@trusted string randomString(uint len)
 {
-    import std.ascii : letters, digits;
-    import std.conv : to;
-    import std.random : randomSample;
-    import std.range : chain;
+	import std.ascii : letters, digits;
+	import std.conv : to;
+	import std.random : randomSample;
+	import std.range : chain;
 
-    auto asciiLetters = to! (dchar[]) (letters);
-    auto asciiDigits = to! (dchar[]) (digits);
+	auto asciiLetters = to!(dchar[])(letters);
+	auto asciiDigits = to!(dchar[])(digits);
 
-    if (len == 0)
-        len = 1;
+	if (len == 0)
+		len = 1;
 
-    auto res = to!string (randomSample (chain (asciiLetters, asciiDigits), len));
-    return res;
+	auto res = to!string(randomSample(chain(asciiLetters, asciiDigits), len));
+	return res;
 }
 
-void main ()
+void main()
 {
-    immutable iterationCount = 4;
-    HashMap!(string, string) hmap;
+	immutable iterationCount = 4;
+	HashMap!(string, string) hmap;
 
-    for (uint n = 1; n <= iterationCount; n++) {
-        foreach (i; 0 .. 1_000_000)
-            hmap[randomString (4)] = randomString (16);
-        GC.collect ();
-        hmap = HashMap!(string, string) (16);
-        GC.collect ();
+	for (uint n = 1; n <= iterationCount; n++)
+	{
+		foreach (i; 0 .. 1_000_000)
+			hmap[randomString(4)] = randomString(16);
+		GC.collect();
+		hmap = HashMap!(string, string)(16);
+		GC.collect();
 
-        foreach (i; 0 .. 1_000_000)
-            hmap[randomString (4)] = randomString (16);
-        GC.collect ();
-        hmap.clear ();
-        GC.collect ();
+		foreach (i; 0 .. 1_000_000)
+			hmap[randomString(4)] = randomString(16);
+		GC.collect();
+		hmap.clear();
+		GC.collect();
 
-        writefln ("iteration %s/%s finished", n, iterationCount);
-    }
+		writefln("iteration %s/%s finished", n, iterationCount);
+	}
 }
